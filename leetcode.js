dd775c42d4e466bfa4763d7d4f54b522eaf56fe4
jomi <64398256+JomiHuang@users.noreply.github.com>
2020-05-03 10:13:29 +0800

    Update leetcode.js

--- leetcode.js
+++ leetcode.js
@@ -26,3 +26,32 @@ var intersect = function(nums1, nums2) {
     }
     return temp;
 };
+
+
+//环形链表
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+
+/**
+ * @param {ListNode} head
+ * @return {boolean}
+ */
+var hasCycle = function(head) {
+    if(!head || !head.next) {
+        return false;
+    }
+    let slow = head, fast = head.next;
+    while(slow !== fast) {
+        if(fast === null || fast.next === null) {
+            return false;
+        }
+        slow = slow.next;
+        fast = fast.next.next;
+    }
+    return true;
+};
